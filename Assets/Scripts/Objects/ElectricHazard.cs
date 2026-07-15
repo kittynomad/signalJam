96b2af443829b08b0dbf56c8fc628e1c99e60d37
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-14 21:16:18 -0500

    filled ElectricHazard

--- Assets/Scripts/Objects/ElectricHazard.cs
+++ Assets/Scripts/Objects/ElectricHazard.cs
@@ -4,15 +4,22 @@ public class ElectricHazard : MonoBehaviour
 {
     bool isToggledOn = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void ToggleActive()
     {
-        
+        isToggledOn = !isToggledOn;
+        UpdateState();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateState()
     {
-        
+        //put visual changes here?
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(isToggledOn && collision.gameObject.TryGetComponent(out IKillable ik))
+        {
+            ik.OnDamage(1, gameObject)
+        }
     }
 }
