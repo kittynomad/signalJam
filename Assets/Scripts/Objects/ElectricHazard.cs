5cad72d6f3342da1eac6b0912481de5a34f4d053
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-15 15:39:57 -0500

    created electric hazard

--- Assets/Scripts/Objects/ElectricHazard.cs
+++ Assets/Scripts/Objects/ElectricHazard.cs
@@ -4,6 +4,11 @@ public class ElectricHazard : MonoBehaviour
 {
     [SerializeField] bool isToggledOn = false;
 
+    private void Start()
+    {
+        UpdateState();
+    }
+
     public void ToggleActive()
     {
         isToggledOn = !isToggledOn;
@@ -13,6 +18,15 @@ public class ElectricHazard : MonoBehaviour
     private void UpdateState()
     {
         //put visual changes here?
+        //im putting placeholder SLOPPP here for now
+        if(isToggledOn)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
