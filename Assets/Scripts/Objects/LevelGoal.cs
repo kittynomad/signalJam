6d7f45341018db630dad473965126daf9b996392
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-16 12:39:36 -0500

    LevelGoal now brings player through objects

--- Assets/Scripts/Objects/LevelGoal.cs
+++ Assets/Scripts/Objects/LevelGoal.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class LevelGoal : MonoBehaviour
 {
     [SerializeField] private float _transitionSpeed;
+    [SerializeField] private float _leeway = 0.5f;
     [SerializeField] private GameObject _nextLevelStartLocation;
 
     private GameObject player;
@@ -12,6 +13,7 @@ public class LevelGoal : MonoBehaviour
     {
         player = FindAnyObjectByType<PlayerBehaviors>().gameObject;
         player.GetComponent<PlayerInput>().DeactivateInput();
+        player.GetComponent<Collider2D>().isTrigger = true;
         StartCoroutine(LevelTransitionCoroutine());
         
     }
@@ -19,11 +21,12 @@ public class LevelGoal : MonoBehaviour
     {
         player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
         player.GetComponent<PlayerInput>().ActivateInput();
+        player.GetComponent<Collider2D>().isTrigger = false;
     }
 
     private IEnumerator LevelTransitionCoroutine()
     {
-        while (Vector2.Distance(player.GetComponent<Rigidbody2D>().position, _nextLevelStartLocation.transform.position) >= 0.5f)
+        while (Vector2.Distance(player.GetComponent<Rigidbody2D>().position, _nextLevelStartLocation.transform.position) >= _leeway)
         {
             player.GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(player.GetComponent<Rigidbody2D>().position, _nextLevelStartLocation.transform.position, _transitionSpeed));
             yield return new WaitForFixedUpdate();
