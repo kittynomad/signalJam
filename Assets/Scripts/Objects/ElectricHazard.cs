e7961b9d0682f014cc590d55902c819a47fb1ad7
VideoDrane <cdrane@mail.bradley.edu>
2026-07-17 23:48:29 -0500

    blugbh

--- Assets/Scripts/Objects/ElectricHazard.cs
+++ Assets/Scripts/Objects/ElectricHazard.cs
@@ -1,12 +1,42 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ElectricHazard : MonoBehaviour
 {
     [SerializeField] bool isToggledOn = false;
 
+    [SerializeField] private GameObject[] _sparkleVFX;
+
+    [SerializeField] private BoxCollider2D _bC2D;
+    [SerializeField] private float _sparkleFreq;
+
     private void Start()
     {
         UpdateState();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isToggledOn)
+        {
+                if (UnityEngine.Random.Range(0, 100) >= _sparkleFreq)
+                {
+                    Sparkle();
+                }
+        }
+    }
+
+    public void Sparkle()
+    {
+        GameObject sparkle = _sparkleVFX[UnityEngine.Random.Range(0, _sparkleVFX.Length - 1)];
+        Vector2 pluh = _bC2D.bounds.size;
+        float x = UnityEngine.Random.Range(-pluh.x, pluh.x) / 2;
+        float y = UnityEngine.Random.Range(-pluh.y, pluh.y) / 2;
+        GameObject newbornSparkle = Instantiate(sparkle, transform.position, Quaternion.identity);
+        newbornSparkle.transform.parent = gameObject.transform;
+        newbornSparkle.transform.position = new Vector2(newbornSparkle.transform.position.x + x, newbornSparkle.transform.position.y + y);
+        newbornSparkle.transform.parent = gameObject.transform;
     }
 
     public void ToggleActive()
