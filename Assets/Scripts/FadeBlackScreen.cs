b0c88e2451c0b2c0b9241b94d0aaee28a80fb749
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-20 13:02:03 -0500

    fartmaster 3000

--- Assets/Scripts/FadeBlackScreen.cs
+++ Assets/Scripts/FadeBlackScreen.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class FadeBlackScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image _blackScreen;
+    [SerializeField] private float _fadeTime;
+
+    private void Start()
     {
-        
+        FadeIn();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FadeIn()
     {
-        
+        StartCoroutine(DoFadeIn(_fadeTime));
+    }
+
+    public void FadeOut(float dur)
+    {
+        StartCoroutine(DoFadeOut(dur));
+    }
+
+    public IEnumerator DoFadeIn(float fadeTime)
+    {
+        _blackScreen.color = new Color(0, 0, 0, 1);
+        while(_blackScreen.color.a > 0)
+        {
+            _blackScreen.color = new Color(0, 0, 0, _blackScreen.color.a - (fadeTime * Time.deltaTime)) ;
+            yield return new WaitForFixedUpdate();
+        }
+        _blackScreen.enabled = false;
+    }
+
+    public IEnumerator DoFadeOut(float fadeTime)
+    {
+        _blackScreen.enabled = true;
+        _blackScreen.color = new Color(0, 0, 0, 0);
+        while (_blackScreen.color.a < 1)
+        {
+            _blackScreen.color = new Color(0, 0, 0, _blackScreen.color.a + (fadeTime * Time.deltaTime));
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
