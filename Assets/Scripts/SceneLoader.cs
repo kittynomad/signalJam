5578486a576b3f66b637527f9d6eb0d60b70afd6
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-20 09:45:50 -0500

    freak the fuck out and panic sell everything

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private float _sceneTransitionTime = 0f;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneDelayed(string sceneName)
+    {
+        StartCoroutine(DelayCoroutine(sceneName));
+    }
+
+    IEnumerator DelayCoroutine(string sceneName)
+    {
+        yield return new WaitForSeconds(_sceneTransitionTime);
+        LoadScene(sceneName);
     }
 }
